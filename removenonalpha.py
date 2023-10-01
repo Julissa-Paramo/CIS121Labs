03583758ae79a6525ea94a47e74df299ca40d234
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-09-30 22:44:37 -0500

    Update removenonalpha.py

--- removenonalpha.py
+++ removenonalpha.py
@@ -3,18 +3,19 @@ Julissa Paramo
 9/30/23
 nonalpha
 '''
+#removes all characters that aren't letters
 
 word = input()
 
 for letter in word:
 
-    num = ord(letter)
+    num = ord(letter) # retreieves the ASCII value of each character in the string
 
-    if 65 <= num <= 90 or 97<= num <= 122:
+    if 65 <= num <= 90 or 97<= num <= 122: # intervals for a-z and A-z
 
-        let = chr(num)
+        let = chr(num) # converts value back to character
         
-        print(f'{let}', end='')
+        print(f'{let}', end='') # prints characters consecutively
         
 print()
         
