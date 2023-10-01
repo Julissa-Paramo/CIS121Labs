e2ca7363224f68013372e9d2e7dd3eed1723faba
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-09-30 20:56:15 -0500

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,21 +3,20 @@ Julissa Paramo
 9/30/23
 palindrome
 '''
+#checks if the input is a palindrome
+#reversing string
+#splitting and joining
 
-word = input()
+word = input() 
 
-word1 = word.split()
+word1 = word.split() # splits words into tokens if there are any
 
-word2 = ''.join(word1)
+word2 = ''.join(word1) # joins any existing tokens together so the string can be read without spaces
 
-
-backward = word2[-1:0:-1] + word[0]
-
-print(backward)
-
+backward = word2[-1:0:-1] + word[0] # reverses the order of the string
 
 if backward == word2:
-    print(f'palindrome: {word}')
+    print(f'palindrome: {word}') # if the reversed order matches the original order, prints palindrome
 
 else:
     print(f'not a palindrome: {word}')
