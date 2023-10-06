59b1d4d3658a88128620472b384dc746b94fccdf
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-10-06 13:57:43 -0500

    Update filter.and.sort.list.py

--- filter.and.sort.list.py
+++ filter.and.sort.list.py
@@ -3,22 +3,26 @@ Julissa Paramo
 10 / 6 / 23
 Filter and Sort a List
 '''
+# returns only the negative values from largest to smallest from an input
+# list to string print statements
+# for loops in lists
 
+nums = input() # numbers are inputted
 
-nums = input()
+nums1 = nums.split() # creates a list of strings from the numbers 
 
-nums1 = nums.split()
+nums2 = list(map(int, nums1)) # makes a new list of integers
 
-nums2 = list(map(int, nums1))
+sortedlist = sorted(nums2) # sorts the list from smallest to largest integrers
 
-sortedlist = sorted(nums2)
+negvals = [] # empty list
 
-negvals = []
+for num in sortedlist: # iterate through each integer in the list
 
-for num in sortedlist:
+    if num < 0: # checks if the integer is negative
 
-    if num < 0:
-
-        negvals.append(num)
+        negvals.append(num) # adds the integer to the empty list
         
-print(' '.join(map(str, negvals[::-1])))
+print(' '.join(map(str, negvals[::-1]))) # negvals[::-1] returns the list in reverse order
+                                        # list is converted to a string with (map(str, x)
+                                        # list is concatenated by ' ' with join function
