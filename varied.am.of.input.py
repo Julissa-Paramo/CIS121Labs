c10d5b5a17911724041da16f9e493e0b689c5df4
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-10-06 14:09:28 -0500

    Update varied.am.of.input.py

--- varied.am.of.input.py
+++ varied.am.of.input.py
@@ -11,11 +11,14 @@ Varies amount of input data
 
 nums = (input()) # takes in input
 
+
 list1 = nums.split() # turns input into a list - still a list of strings
 
 list2 = list(map(float, list1)) # makes a new list , converts strings into floats
 
+
 sortednums = sorted(list2) # creates a new sorted list
+
 
 sum = 0 # starting sum for calculating the avg
 
@@ -24,5 +27,6 @@ for num in list2: # for loop goes through the list of floats
     sum = sum + num # running sum
 
 avg = sum / len(list2) # calculates avg: sum / length of the list
+
 
 print(f'{sortednums[-1]:.2f} {avg:.2f}') # prints the max number, and the average
