8469366f5751305c16adb2a84af51b5633a33aaa
Juli <143529579+Julissa-Paramo@users.noreply.github.com>
2023-10-10 21:31:47 -0500

    Update car.wash.py

--- zb.dictionaries/car.wash.py
+++ zb.dictionaries/car.wash.py
@@ -6,27 +6,27 @@ Car Wash
 # matching input with keys in a dictionary 
 # for loops in dictionaries
 
-services = { 'Air freshener' : 1 , 'Rain repellent': 2, 'Tire shine' : 2, 'Wax' : 3, 'Vacuum' : 5 }
+services = { 'Air freshener' : 1 , 'Rain repellent': 2, 'Tire shine' : 2, 'Wax' : 3, 'Vacuum' : 5 } # dictionary for services and their cost
 
-service_choice1 = input()
+service_choice1 = input() # first service 
 
-service_choice2 = input()
+service_choice2 = input() # second service
 
-print('ZyCar Wash\nBase car wash - $10')
+print('ZyCar Wash\nBase car wash - $10') # first two printed statements
 
-sum1 = 0
+sum1 = 0 # cost of first service
 
-for choice in services.keys():
+for choice in services.keys(): # iterates through the keys in the dictionaries
 
-    if choice == service_choice1:
+    if choice == service_choice1: # if the key name matches the service from the input then . . .
 
-        sum1 = sum1 + services[choice]
+        sum1 = sum1 + services[choice] # adds the value from the key in the dictionary to the sum
 
-        print(f'{service_choice1} - ${(services[choice])}')
+        print(f'{service_choice1} - ${(services[choice])}') # prints the service and its total
 
-sum2 = 0
+sum2 = 0 # cost of second service
 
-for choice in services.keys():
+for choice in services.keys(): 
 
     if choice == service_choice2:
 
@@ -35,4 +35,5 @@ for choice in services.keys():
         print(f'{service_choice2} - ${(services[choice])}')
 
 
-print(f'-----\nTotal price: ${10 + sum1 + sum2}')
+print(f'-----\nTotal price: ${10 + sum1 + sum2}') # adds the price of the Base car wash from line 15 to the two service costs from the for loops
+                                                    # inserts dashes above statement for organization
